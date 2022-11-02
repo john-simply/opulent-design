ef2b50b8b056c1efac602070ad9f97cd7582a8ec
John <john@jpann.co.uk>
2022-11-02 10:17:04 +0000

    lift animation complete for homepage

---
+++ scripts/gsap-animations.js
@@ -0,0 +1,75 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const liftAnimation = (direction) => {
+  const isScrollingDown = direction === 1;
+  console.log('yes');
+  return gsap.to('#product-lift', {
+    y: isScrollingDown ? '100vh' : '0vh',
+    transformOrigin: 'top bottom',
+    ease: 'power1',
+  });
+};
+
+const arrowAnimation = (direction) => {
+  const isScrollingDown = direction === 1;
+  console.log('yes');
+  return gsap.to('.main-content .lift-wrapper__buttons', {
+    y: isScrollingDown ? '100vh' : '0vh',
+
+    transformOrigin: 'top bottom',
+    ease: 'none',
+  });
+};
+
+const liftScrollInit = () => {
+  ScrollTrigger.create({
+    trigger: '.suppliers-block__content',
+    start: 'top-=300 center',
+    end: 'bottom bottom',
+
+    onEnter: ({ direction }) => {
+      liftAnimation(direction);
+
+      // animate arrows depending on screen size
+      if (window.screen.width >= 1024 && window.screen.width < 1440) {
+        const arrows = document.querySelector(
+          '.main-content .lift-wrapper__buttons'
+        );
+        // arrows.style.transform = 'rotate(-90deg) translate(-100vh, 0vh)';
+        setTimeout(() => {
+          arrows.style.transform = 'translateY(100vh)';
+        }, 500);
+      } else {
+        const arrows = document.querySelector(
+          '.main-content .lift-wrapper__buttons'
+        );
+        arrows.style.transform = 'rotate(-90deg) translate(-100vh, 0vh)';
+      }
+    },
+    onLeaveBack: ({ direction }) => {
+      liftAnimation(direction);
+
+      // animate arrows depending on screen size
+      if (window.screen.width >= 1024 && window.screen.width < 1440) {
+        const arrows = document.querySelector(
+          '.main-content .lift-wrapper__buttons'
+        );
+        // arrows.style.transform = 'rotate(-90deg) translate(-100vh, 0vh)';
+        setTimeout(() => {
+          arrows.style.transform = 'translateY(0vh)';
+        }, 500);
+      } else {
+        const arrows = document.querySelector(
+          '.main-content .lift-wrapper__buttons'
+        );
+        arrows.style.transform = 'rotate(-90deg) translate(0vh, 0vh)';
+      }
+    },
+  });
+};
+
+window.addEventListener('load', () => {
+  if (document.querySelector('.lift-wrapper')) {
+    liftScrollInit();
+  }
+});
